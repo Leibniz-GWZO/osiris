93a6e3afdbc5ed556ce69557f58a2a45e966cfea
Jonathan S. <49925310+jsektkuehler@users.noreply.github.com>
2025-10-13 15:05:03 +0200

    Create head-scripts.php
    Created file to add snippets to the head section, e.g. plausible tracking.

---
+++ addons/head-scripts.php
@@ -0,0 +1,6 @@
+<script defer data-domain="osiris.gwzo-fis.de" src="https://stats.leibniz-gwzo.de/js/script.file-downloads.hash.outbound-links.pageview-props.tagged-events.js"></script>
+<script>
+    window.plausible = window.plausible || function () {
+        (window.plausible.q = window.plausible.q || []).push(arguments)
+    }
+</script>
